407ac4a9b6f5054dce72f44391b2b4f3c43d95c6
Abdelrhman Tarek <47614199+Abdelrhman-T@users.noreply.github.com>
2023-05-21 16:35:25 +0300

    Update admindeletebranch.Designer.cs

--- BankSystem/admindeletebranch.Designer.cs
+++ BankSystem/admindeletebranch.Designer.cs
@@ -1,193 +1,84 @@
-﻿namespace BankSystem
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankSystem
 {
-    partial class admindeletebranch
+    public partial class admindeletebranch : Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public admindeletebranch()
         {
-            if (disposing && (components != null))
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=BODA;Initial Catalog=Bank_System;Integrated Security=True");
+            string branchtodelete = b_code_input.Text;
+
+            string sqlDelete = "DELETE FROM Branch WHERE BranchNumber = @bNumber";
+            SqlCommand cmd = new SqlCommand(sqlDelete, sqlConnection);
+            cmd.Parameters.AddWithValue("@bNumber", branchtodelete);
+
+            sqlConnection.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            if (rowsAffected > 0)
             {
-                components.Dispose();
+                MessageBox.Show("The branch has been deleted successfully");
             }
-            base.Dispose(disposing);
+            else
+            {
+                MessageBox.Show("No branch with the provided BankCode was found");
+            }
+
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+        private void button1_Click(object sender, EventArgs e)
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(admindeletebranch));
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.b_code_input = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(19, 469);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(305, 120);
-            this.button5.TabIndex = 34;
-            this.button5.Text = "Update Branch";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(19, 650);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(305, 120);
-            this.button4.TabIndex = 33;
-            this.button4.Text = "Delete Branch";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(19, 289);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(305, 120);
-            this.button2.TabIndex = 29;
-            this.button2.Text = "Add a branch";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(19, 102);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(305, 120);
-            this.button1.TabIndex = 28;
-            this.button1.Text = "Add a new bank";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // b_code_input
-            // 
-            this.b_code_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.b_code_input.Location = new System.Drawing.Point(709, 371);
-            this.b_code_input.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.b_code_input.Name = "b_code_input";
-            this.b_code_input.Size = new System.Drawing.Size(357, 35);
-            this.b_code_input.TabIndex = 26;
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(624, 208);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(551, 95);
-            this.label2.TabIndex = 24;
-            this.label2.Text = "Enter the Branch Number you want to delete";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(712, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(354, 95);
-            this.label1.TabIndex = 23;
-            this.label1.Text = "Delete a branch";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(344, 1074);
-            this.listView1.TabIndex = 32;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(19, 990);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(305, 61);
-            this.button3.TabIndex = 35;
-            this.button3.Text = "Log out";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(738, 499);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(294, 60);
-            this.button6.TabIndex = 36;
-            this.button6.Text = "Delete Branch";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // admindeletebranch
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1420, 1066);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.b_code_input);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "admindeletebranch";
-            this.Text = "admindeletebank";
-            this.Load += new System.EventHandler(this.admindeletebranch_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            adminaddbank adminaddbank = new adminaddbank();
+            adminaddbank.Show();
+            this.Hide();
 
         }
 
-        #endregion
+        private void button2_Click(object sender, EventArgs e)
+        {
+            adminaddbranch adminaddbranch = new adminaddbranch();
+            adminaddbranch.Show();
+            this.Hide();
+        }
 
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox b_code_input;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button6;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            adminupdatebranch adminupdatebranch = new adminupdatebranch();
+            adminupdatebranch.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            adminlogin adminlogin = new adminlogin();
+            adminlogin.Show();
+            this.Hide();
+        }
+
+        private void admindeletebranch_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
